a7c55cbc6e780e5bd947240b5c495b92e347ed49
Tschorsch, Danny <danny.tschorsch@atos.net>
2019-03-01 17:11:15 +0100

    added camera

--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,33 +1,55 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.css']
 })
-export class ScanComponent implements OnInit {
-
+export class ScanComponent implements OnInit, OnDestroy {
   @ViewChild('video') video: ElementRef;
   @ViewChild('canvas') canvas: ElementRef;
 
-  constructor() { }
+  videoElement: HTMLVideoElement;
+  canvasElement: HTMLCanvasElement;
+  stream: MediaStream;
+
+  constructor() {}
 
   ngOnInit() {
+    this.videoElement = this.video.nativeElement;
+    this.canvasElement = this.canvas.nativeElement;
+    this.initCamera();
   }
 
-  initCamera() {
-    navigator.getUserMedia = navigator.getUserMedia || navigator.mediaDevices.getUserMedia;
+  ngOnDestroy() {
+    if (this.stream) {
+      this.stream.stop();
+    }
+  }
 
-    navigator.getUserMedia({
-      video: {
-        facingMode: 'environment'
+  async initCamera() {
+    navigator.getUserMedia =
+      navigator.getUserMedia || navigator.mediaDevices.getUserMedia;
+
+    navigator.getUserMedia(
+      {
+        video: {
+          facingMode: { ideal: 'environment' }
+        }
+      },
+      res => {
+        this.stream = res;
+        this.videoElement.srcObject = this.stream;
+      },
+      err => {
+        alert(err);
       }
-    }, (res) => {
-      console.log({ result: res });
-    }, err => {
-      alert(err);
-    });
-
+    );
   }
-
 }
